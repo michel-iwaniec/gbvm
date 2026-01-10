383f91c68a5df608305c8fc695edbfaed3f2b137
Michel Iwaniec <michel_iwaniec@yahoo.com>
2026-01-10 22:47:46 +0000

    Optimization: Change actor flags from bitfield-bools to "classic" bitmask accessed via SET/CLR/CHK_FLAG macro

--- include/vm_actor.h
+++ include/vm_actor.h
@@ -34,12 +34,6 @@ void vm_actor_get_dir(SCRIPT_CTX * THIS, INT16 idx, INT16 dest) OLDCALL BANKED;
 void vm_actor_get_angle(SCRIPT_CTX * THIS, INT16 idx, INT16 dest) OLDCALL BANKED;
 void vm_actor_set_anim_tick(SCRIPT_CTX * THIS, INT16 idx, UBYTE speed) OLDCALL BANKED;
 void vm_actor_set_move_speed(SCRIPT_CTX * THIS, INT16 idx, UBYTE speed) OLDCALL BANKED;
-
-#define ACTOR_FLAG_PINNED       0x01
-#define ACTOR_FLAG_HIDDEN       0x02
-#define ACTOR_FLAG_ANIM_NOLOOP  0x04
-#define ACTOR_FLAG_COLLISION    0x08
-#define ACTOR_FLAG_PERSISTENT   0x10
 void vm_actor_set_flags(SCRIPT_CTX * THIS, INT16 idx, UBYTE flags, UBYTE mask) OLDCALL BANKED;
 
 void vm_actor_begin_update(SCRIPT_CTX * THIS, INT16 idx) OLDCALL BANKED;
