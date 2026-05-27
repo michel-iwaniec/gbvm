ba11819ee846eb12a6831bb6d031fd7df2044315
Chris Maltby <chris.maltby@gmail.com>
2026-05-27 17:52:07 +0100

    Add tests for infinite wall jump bug when coyote time on and prevented wall jump bug when coyote time on but set to 0

---
+++ test/wall-jump-prevented-bug/data/include/data/bg_jump_bug_scene_tilemap.h
@@ -0,0 +1,11 @@
+#ifndef BG_JUMP_BUG_SCENE_TILEMAP_H
+#define BG_JUMP_BUG_SCENE_TILEMAP_H
+
+// Tilemap bg_jump_bug_scene_tilemap
+
+#include "gbs_types.h"
+
+BANKREF_EXTERN(bg_jump_bug_scene_tilemap)
+extern const unsigned char bg_jump_bug_scene_tilemap[];
+
+#endif
---
+++ test/wall-jump-prevented-bug/data/include/data/bg_jump_bug_scene_tileset.h
@@ -0,0 +1,11 @@
+#ifndef BG_JUMP_BUG_SCENE_TILESET_H
+#define BG_JUMP_BUG_SCENE_TILESET_H
+
+// Tileset: bg_jump_bug_scene_tileset
+
+#include "gbs_types.h"
+
+BANKREF_EXTERN(bg_jump_bug_scene_tileset)
+extern const struct tileset_t bg_jump_bug_scene_tileset;
+
+#endif
